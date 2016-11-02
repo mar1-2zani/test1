3bc8bee1373e58bb321c21a57a7ab0261c958f30
Rajith Delantha <rajithgama@gmail.com>
2016-11-02 15:17:53 +0530

    Initial commit

---
+++ src/main/java/App1.java
@@ -0,0 +1,13 @@
+/**
+ * Created by rajith on 11/2/16.
+ */
+public class App1 {
+
+    public void exe(){
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+
+}
